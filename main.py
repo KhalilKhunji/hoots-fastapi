45b635eb65b586c1692741ebb699377b24f86f45
Khalil Khunji <khalilkhunji@gmail.com>
2025-12-13 12:09:24 +0300

    added cors

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from controllers.users import router as UsersRouter
 from controllers.hoots import router as HootsRouter  # NEW
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Hoot API",
     description="A blogging platform API built with FastAPI",
     version="1.0.0"
+)
+
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 # Register routers
